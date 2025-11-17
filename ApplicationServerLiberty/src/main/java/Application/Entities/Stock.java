c88b05575910ac7a9d45e65eb44437ec67f44b55
Nesreyli <rajicicmarko1@gmail.com>
2025-11-17 09:32:37 -0500

    Switched name to company for easier understanding of entities.

--- ApplicationServerLiberty/src/main/java/Application/Entities/Stock.java
+++ ApplicationServerLiberty/src/main/java/Application/Entities/Stock.java
@@ -5,22 +5,22 @@ import jakarta.enterprise.context.RequestScoped;
 @RequestScoped
 public class Stock {
     private String symbol;
-    private String name;
+    private String company;
     private double price;
     private String country;
 
     public Stock() {}
 
-    public Stock(String symbol, String name, double price, String country) {
+    public Stock(String symbol, String company, double price, String country) {
         this.symbol = symbol;
-        this.name = name;
+        this.company = company;
         this.price = price;
         this.country = country;
     }
 
     public String getSymbol() {return symbol;}
 
-    public String getName() {return name;}
+    public String getCompany() {return company;}
 
     public double getPrice() { return price; }
 
