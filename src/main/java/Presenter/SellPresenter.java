b2f8018d8da44bcbd00b1f345178a98337b5ebf6
lucas-user <lucasth.chan@mail.utoronto.ca>
2025-11-17 09:46:15 -0500

    Refactoring

--- src/main/java/Presenter/SellPresenter.java
+++ src/main/java/Presenter/SellPresenter.java
@@ -1,4 +1,4 @@
-package Application.Presenter;
+package Presenter;
 
 import Application.UseCases.Sell.OutputDataSell;
 
--- src/main/java/Presenter/UserPresenter.java
+++ src/main/java/Presenter/UserPresenter.java
@@ -1,4 +1,4 @@
-package Application.Presenter;
+package Presenter;
 
 import Application.UseCases.User.OutputDataSignup;
 
